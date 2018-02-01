5072e5685451b18b66fc0a4f8581e7d802a6c103
Christian Fritsch <chr.fritsch@gmx.net>
2018-02-01 09:16:51 +0100

    Add drush aliases

--- zsh-custom/aliases.zsh
+++ zsh-custom/aliases.zsh
@@ -1,1 +1,4 @@
 alias dr="drush"
+
+alias drupalcs="phpcs --standard=Drupal --extensions='php,module,inc,install,test,profile,theme' --ignore='*.md,*.txt,node_modules,libraries -p'"
+alias drupalcsp="phpcs --standard=DrupalPractice --extensions='php,module,inc,install,test,profile,theme,css,info,txt,md'"
