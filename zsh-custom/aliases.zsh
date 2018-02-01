cd588eb7702fc74b5c55176798d4478c87908fd9
Christian Fritsch <chr.fritsch@gmx.net>
2018-02-01 10:22:07 +0100

    Add gitconfig and inputrc

--- zsh-custom/aliases.zsh
+++ zsh-custom/aliases.zsh
@@ -2,3 +2,5 @@ alias dr="drush"
 
 alias drupalcs="phpcs --standard=Drupal --extensions='php,module,inc,install,test,profile,theme' --ignore='*.md,*.txt,node_modules,libraries -p'"
 alias drupalcsp="phpcs --standard=DrupalPractice --extensions='php,module,inc,install,test,profile,theme,css,info,txt,md'"
+
+alias drupal-phantomjs="phantomjs --ssl-protocol=any --ignore-ssl-errors=true vendor/jcalderonzumba/gastonjs/src/Client/main.js 8510 1024 768"
